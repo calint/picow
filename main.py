7f67deaaf60b7ebbfc8d1be87c30506248fd3f0b
calint <calin.tenitchi@gmail.com>
2023-08-16 17:32:02 +0200

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import machine
 import network
 import secrets
 import time
@@ -18,3 +19,12 @@ astronauts = urequests.get("http://api.open-notify.org/astros.json").json()
 
 for i in range(astronauts['number']):
     print(astronauts['people'][i]['name'])
+
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+
+while True:
+    reading = sensor_temp.read_u16() * conversion_factor 
+    temperature = 27 - (reading - 0.706)/0.001721
+    print(temperature)
+    time.sleep(2)
