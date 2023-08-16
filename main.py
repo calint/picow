51946b6f8d1896de2036f96cd8591da79c69cb98
calint <calin.tenitchi@gmail.com>
2023-08-16 16:28:00 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import network
+import secrets
+import time
+import urequests
 from machine import Pin
-from utime import sleep
 
-pin = Pin("LED", Pin.OUT)
+led = Pin("LED", Pin.OUT)
+led.off()
 
-print(pin)
-while True:
-    pin.toggle()
-    sleep(1)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(secrets.SSID, secrets.PASSWORD)
+
+if wlan.isconnected():
+    led.on()
+
+astronauts = urequests.get("http://api.open-notify.org/astros.json").json()
+
+for i in range(astronauts['number']):
+    print(astronauts['people'][i]['name'])
---
+++ secrets.py.template
@@ -0,0 +1,2 @@
+SSID="network"
+PASSWORD="password"
